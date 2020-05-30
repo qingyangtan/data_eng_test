31fd2bb0148fee5c1e468f77815af3ec6e7cb28b
Yang <tanqyang@gmail.com>
2020-05-30 11:59:23 +0800

    Section 1: Add pipeline processing script

---
+++ Section 1/de_pipeline/scripts/pipeline_1.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+def get_first_last_name(name):
+    """Return a person first and last name"""
+    name = name.split()
+    name = [x for x in name if '.' not in x]
+    if len(name) == 2:
+        return name
+    if len(name) == 3:
+        return name[:-1]
+    if len(name) == 4:
+        return name[1:-1]
+    return None, None
+
+
+def get_above_100(price):
+    """Return price that is above 100 as True"""
+    if price > 100:
+        return True
+    return False
+
+
+def remove_prepend_zeros(price):
+    """Return price without zeros prepended"""
+    return str(price).strip("0")
+
+
+def main(df):
+    # Process first and last name
+    df['first_name'], df['last_name'] = zip(*df['name'].apply(get_first_last_name))
+
+    # Drop rows that are empty or None
+    df = df[(df['name'] != '') | (df['name'] is not None)]
+
+    # Get prices above 100
+    df['above_100'] = df['price'].apply(get_above_100)
+
+    # Remove preprend 0
+    df['price'] = df['price'].apply(remove_prepend_zeros)
+
+    print(len(df))
+    print(df.head(10))
+
+    df.to_csv('../../data/output/p_dataset.csv', index=False)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv('../../data/input/dataset.csv')
+    main(df)
